8557cff50bdf67ff742e9b037a4c8f31905828f1
Aen Jojo <josuafernando999@gmail.com>
2022-12-02 12:48:41 +0700

    feat: add read directory function

---
+++ src/libs/readDirectory.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs/promises';
+import { URL } from 'node:url';
+
+export default async function readDirectory(directory: string) {
+	const directoryPath = new URL(directory, import.meta.url);
+	const directoryFiles = await fs.readdir(directoryPath);
+	const filteredFiles = directoryFiles.filter(file => file.endsWith('.js'));
+
+	return {
+		directoryPath,
+		filteredFiles,
+	};
+}
