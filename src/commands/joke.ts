2127667ee323565c0f532b8444cda644343c8c30
Aen Jojo <josuafernando999@gmail.com>
2022-12-18 19:17:48 +0700

    feat: add joke command

---
+++ src/commands/joke.ts
@@ -0,0 +1,34 @@
+import { NexClient } from '#core/NexClient';
+import { NexCommand } from '#core/NexCommand';
+import { fetchData } from '#services/fetchData';
+import {
+	ChatInputCommandInteraction,
+	SlashCommandBuilder,
+} from 'discord.js';
+import config from '#root/config';
+
+interface ResultType {
+	joke: string,
+}
+
+export class AppCommand extends NexCommand {
+	constructor(client?: NexClient) {
+		super(client);
+	}
+
+	buildApplicationCommand() {
+		return new SlashCommandBuilder()
+			.setName('joke')
+			.setDescription('Sends a random joke');
+	}
+
+	async executeApplicationCommand(interaction: ChatInputCommandInteraction) {
+		await interaction.deferReply();
+
+		const result: ResultType = await fetchData({
+			url: config.api.icanhazdadjoke,
+		});
+
+		await interaction.editReply(result.joke);
+	}
+}
