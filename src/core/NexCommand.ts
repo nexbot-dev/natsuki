3910b092d107124bf591851876971b6a28661d8b
Aen Jojo <josuafernando999@gmail.com>
2023-01-27 12:48:54 +0700

    fix: remove interaction prop from command class

--- src/core/NexCommand.ts
+++ src/core/NexCommand.ts
@@ -3,11 +3,9 @@ import { NexClient } from '#core/NexClient';
 
 export class NexCommand {
 	public readonly client?: NexClient;
-	public interaction?: CommandInteraction;
 
 	public constructor(client?: NexClient) {
 		this.client = client;
-		this.interaction;
 
 		return this;
 	}
