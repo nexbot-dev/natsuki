1262d4d7ef47152e3e840251e5b4fc702c8fa011
Aen Jojo <josuafernando999@gmail.com>
2023-03-25 18:42:32 +0700

    feat: add 36 gif commands
    those commands are powered by `nekos.best` api intead of `ram.moe`

---
+++ src/commands/thumbsup.ts
@@ -0,0 +1,8 @@
+import { NexClient } from '#core/NexClient';
+import { NekoBestCommand } from '#components/Commands/nekosBest';
+
+export class AppCommand extends NekoBestCommand {
+	constructor(client?: NexClient) {
+		super('thumbsup', client);
+	}
+}
