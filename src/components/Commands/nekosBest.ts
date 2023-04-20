d0faf947989e6f2a34c2444a55f89bf140226780
Aen Jojo <josuafernando999@gmail.com>
2023-04-20 23:51:03 +0700

    feat: add message to personalize action

--- src/components/Commands/nekosBest.ts
+++ src/components/Commands/nekosBest.ts
@@ -6,20 +6,26 @@ import {
 	SlashCommandBuilder,
 } from 'discord.js';
 import { GifType, getNekosBest } from '#services/getNekosBest';
+import { UserOption } from '#components/CommandOption/user';
 
 export class NekoBestCommand extends NexCommand {
 	public gifType: GifType;
+	public gifText: string;
 
-	constructor(gifType: GifType, client?: NexClient) {
+	constructor(gifType: GifType, gifText: string, client?: NexClient) {
 		super(client);
 
 		this.gifType = gifType;
+		this.gifText = gifText;
 	}
 
 	buildApplicationCommand() {
 		return new SlashCommandBuilder()
 			.setName(this.gifType)
-			.setDescription(`Sends ${this.gifType} gif from anime`) as SlashCommandBuilder;
+			.setDescription(`Sends ${this.gifType} gif from anime`)
+			.addUserOption(UserOption({
+				isRequired: true,
+			})) as SlashCommandBuilder;
 	}
 
 	async executeApplicationCommand(interaction: ChatInputCommandInteraction) {
@@ -28,8 +34,17 @@ export class NekoBestCommand extends NexCommand {
 		const nekosbest = await getNekosBest(this.gifType);
 		const neko = nekosbest.results[0];
 
+		const user1 = interaction.user.username;
+		const user2 = interaction.options.getUser('user')?.username;
+
+		if (!user2) return;
+
+		const titleText = this.gifText
+			.replace('$user1', user1)
+			.replace('$user2', user2);
+
 		const embed = new EmbedBuilder()
-			.setTitle(this.gifType)
+			.setTitle(titleText)
 			.setColor(0xEE1280)
 			.setDescription(`Source: ${neko.anime_name}`)
 			.setImage(neko.url)
